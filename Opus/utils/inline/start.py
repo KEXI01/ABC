d8b7bcf9fc0f2c656116260e1f429b1534b654a3
K Σ X ! <167321927+KEXI01@users.noreply.github.com>
2025-04-18 20:59:46 +0530

    Update start.py

--- Opus/utils/inline/start.py
+++ Opus/utils/inline/start.py
@@ -35,22 +35,10 @@ def start_pannel(_):
 def private_panel(_,):
     buttons = [
         [InlineKeyboardButton(text=_["S_B_1"],url=f"https://t.me/{app.username}?startgroup=true",)],
+        [
+            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text=_["S_B_9"], url=config.SUPPORT_CHAT),           
+        ],
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
     ]
-    if SUPPORT_CHANNEL and SUPPORT_GROUP:
-        buttons.append(
-            [
-                InlineKeyboardButton(text=_["S_B_6"], url=f"{SUPPORT_CHANNEL}"),
-                InlineKeyboardButton(text=_["S_B_2"], url=f"{SUPPORT_GROUP}"),
-            ]
-        )
-    else:
-        if SUPPORT_CHANNEL:
-            buttons.append(
-                [InlineKeyboardButton(text=_["S_B_6"], url=f"{SUPPORT_CHANNEL}")]
-            )
-        if SUPPORT_GROUP:
-            buttons.append(
-                [InlineKeyboardButton(text=_["S_B_2"], url=f"{SUPPORT_GROUP}")]
-            )
     return buttons
