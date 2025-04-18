d818c2b528e8792f0590ab785dbc1c509249202c
K Σ X ! <167321927+KEXI01@users.noreply.github.com>
2025-04-18 19:43:26 +0530

    Update start.py

--- Opus/utils/inline/start.py
+++ Opus/utils/inline/start.py
@@ -3,15 +3,31 @@ from pyrogram.types import InlineKeyboardButton
 import config
 from Opus import app
 
-def start_panel(_):
+def start_pannel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true&admin=delete_messages+invite_users"
+                text=_["S_B_1"],
+                url=f"https://t.me/{app.username}?start=help",
             ),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),
         ],
     ]
+    if SUPPORT_CHANNEL and SUPPORT_GROUP:
+        buttons.append(
+            [
+                InlineKeyboardButton(text=_["S_B_6"], url=f"{SUPPORT_CHANNEL}"),
+                InlineKeyboardButton(text=_["S_B_2"], url=f"{SUPPORT_GROUP}"),
+            ]
+        )
+    else:
+        if SUPPORT_CHANNEL:
+            buttons.append(
+                [InlineKeyboardButton(text=_["S_B_6"], url=f"{SUPPORT_CHANNEL}")]
+            )
+        if SUPPORT_GROUP:
+            buttons.append(
+                [InlineKeyboardButton(text=_["S_B_2"], url=f"{SUPPORT_GROUP}")]
+            )
     return buttons
 
 
